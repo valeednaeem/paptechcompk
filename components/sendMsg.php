09ef70afba5a38ddee172ba7f77678e457bdd287
Valeed Naeem <valeednaeem@gmail.com>
2024-05-30 12:16:14 +0500

    Update sendMsg.php

--- components/sendMsg.php
+++ components/sendMsg.php
@@ -1,12 +1,12 @@
 <?php
-
+/*
     header('Access-Control-Allow-Origin: localhost');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
     header('Access-Control-Allow-Headers: token, Content-Type');
     header('Access-Control-Max-Age: 1728000');
     header('Content-Length: 0');
     header('Content-Type: text/plain');
-
+*/
 
     $to             = "info@paptech.com.pk";
     $subject        = "Client Query from $name - Paptech IT Solutions";
